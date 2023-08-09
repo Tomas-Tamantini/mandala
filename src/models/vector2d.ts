db4bcfc349f0cf8bcd634be92d6ebd8a0f924fea
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-08-09 13:59:00 -0300

    Refactor vector class to access x, y properties

--- src/models/vector2d.ts
+++ src/models/vector2d.ts
@@ -1,28 +1,28 @@
 export default class Vector2D {
-  constructor(private _x: number, private _y: number) {}
+  constructor(public readonly x: number, public readonly y: number) {}
 
   public get coordinates(): [number, number] {
-    return [this._x, this._y];
+    return [this.x, this.y];
   }
 
   public minus(otherVector: Vector2D): Vector2D {
-    return new Vector2D(this._x - otherVector._x, this._y - otherVector._y);
+    return new Vector2D(this.x - otherVector.x, this.y - otherVector.y);
   }
 
   public plus(otherVector: Vector2D): Vector2D {
-    return new Vector2D(this._x + otherVector._x, this._y + otherVector._y);
+    return new Vector2D(this.x + otherVector.x, this.y + otherVector.y);
   }
 
   public times(scalar: number): Vector2D {
-    return new Vector2D(this._x * scalar, this._y * scalar);
+    return new Vector2D(this.x * scalar, this.y * scalar);
   }
 
   public magnitudeSquared(): number {
-    return this._x * this._x + this._y * this._y;
+    return this.x * this.x + this.y * this.y;
   }
 
   public unit(): Vector2D {
     const magnitude = Math.sqrt(this.magnitudeSquared());
-    return new Vector2D(this._x / magnitude, this._y / magnitude);
+    return new Vector2D(this.x / magnitude, this.y / magnitude);
   }
 }
