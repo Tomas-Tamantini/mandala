8e7a6743551d599b12a341e1954d6f67cea69249
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-08-09 14:54:03 -0300

    Get world dimensions

--- src/models/world.ts
+++ src/models/world.ts
@@ -17,4 +17,28 @@ export default class World {
   public get creatures(): Creature[] {
     return this._creatures;
   }
+
+  public get minXCoord(): number {
+    return Math.min(...this._creatures.map((creature) => creature.position.x));
+  }
+
+  public get maxXCoord(): number {
+    return Math.max(...this._creatures.map((creature) => creature.position.x));
+  }
+
+  public get minYCoord(): number {
+    return Math.min(...this._creatures.map((creature) => creature.position.y));
+  }
+
+  public get maxYCoord(): number {
+    return Math.max(...this._creatures.map((creature) => creature.position.y));
+  }
+
+  public get width(): number {
+    return this.maxXCoord - this.minXCoord;
+  }
+
+  public get height(): number {
+    return this.maxYCoord - this.minYCoord;
+  }
 }
