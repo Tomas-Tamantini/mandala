9ab8b7f3160cd3f27c22d9900aafc4142783ffe3
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-08-09 16:53:54 -0300

    Add canvas and create animation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import draw from "./drawMethods";
+import { cameraForWorld } from "./models/camera";
+
+import mandalaWorld from "./samples/mandalaWorld";
+
+const canvas = document.getElementById("animation-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+let world = mandalaWorld(30, 7, 2);
+const camera = cameraForWorld(canvas.width, canvas.height, world);
+
+function animate() {
+  draw(ctx, world, camera, canvas.width, canvas.height);
+  requestAnimationFrame(animate);
+}
+
+animate();
