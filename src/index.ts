c785032909ab8acc394a746437ad9f1292df9ddc
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-08-14 09:44:32 -0300

    Generate random mandala

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
 import draw from "./drawMethods";
 import { cameraForWorld } from "./models/camera";
 
-import mandalaWorld from "./samples/mandalaWorld";
+import { randomMandalaWorld } from "./samples/mandalaWorld";
 
 const canvas = document.getElementById("animation-canvas") as HTMLCanvasElement;
 const framerateSlider = document.getElementById(
   "framerate-slider"
 ) as HTMLInputElement;
+const randomizeButton = document.getElementById(
+  "random-mandala-btn"
+) as HTMLButtonElement;
 
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 let frameRate = parseInt(framerateSlider.value);
+
+randomizeButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  world = randomMandalaWorld();
+});
 
 framerateSlider.addEventListener("input", () => {
   frameRate = parseInt(framerateSlider.value);
 });
 
-let world = mandalaWorld(30, 7, 2);
+let world = randomMandalaWorld();
 const camera = cameraForWorld(canvas.width, canvas.height, world);
 
 function animate() {
--- src/samples/mandalaWorld.ts
+++ src/samples/mandalaWorld.ts
@@ -1,7 +1,7 @@
 import World from "../models/world";
 import circularCreatures from "./circularCreatures";
 
-export default function mandalaWorld(
+export function mandalaWorld(
   symmetry: number,
   pointMultiplicity: number,
   chaseStepMultiplicity: number = 1
@@ -18,4 +18,34 @@ export default function mandalaWorld(
     chaseRelations.push(chaseRelation);
   }
   return new World(creatures, chaseRelations);
+}
+
+export function randomMandalaWorld(): World {
+  const symmetry = randInt(2, 30);
+  let pointMultiplicity = -1;
+  while (true) {
+    pointMultiplicity = randInt(2, 50);
+    let numPoints = pointMultiplicity * symmetry;
+    if (10 <= numPoints && numPoints <= 200) break;
+  }
+  let chaseStepMultiplicity = -1;
+  while (true) {
+    chaseStepMultiplicity = randInt(1, pointMultiplicity - 1);
+    if (isCoprime(chaseStepMultiplicity, pointMultiplicity)) break;
+  }
+  return mandalaWorld(symmetry, pointMultiplicity, chaseStepMultiplicity);
+}
+
+function randInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isCoprime(a: number, b: number): boolean {
+  return gcd(a, b) === 1;
+}
+
+function gcd(a: number, b: number): number {
+  if (b > a) return gcd(b, a);
+  if (b === 0) return a;
+  return gcd(b, a % b);
 }
