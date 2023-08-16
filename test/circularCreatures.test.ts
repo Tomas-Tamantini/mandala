350cd3390ce0ed142b98f7d3f48c143179493d6c
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-08-16 09:58:14 -0300

    Write unit tests for the circularCreatures method

---
+++ test/circularCreatures.test.ts
@@ -0,0 +1,23 @@
+import Vector2D from "../src/models/vector2d";
+import circularCreatures from "../src/samples/circularCreatures";
+
+describe("circularCreatures function", () => {
+  it("should return the correct number of creatures", () => {
+    const numCreatures = 5;
+    const creatures = circularCreatures(numCreatures);
+    expect(creatures).toHaveLength(numCreatures);
+  });
+
+  it("should place creatures in a circle with the given radius and center", () => {
+    const numCreatures = 8;
+    const radius = 100;
+    const center = new Vector2D(50, 50);
+    const creatures = circularCreatures(numCreatures, radius, center);
+
+    creatures.forEach((creature) => {
+      const dx = creature.position.x - center.x;
+      const dy = creature.position.y - center.y;
+      expect(dx * dx + dy * dy).toBeCloseTo(radius * radius);
+    });
+  });
+});
